7b5afd7a681bf777f24bf709404ceee605eda1a1
gyuzelova <tanq.nikolova.tn@gmail.com>
2024-03-26 22:18:11 +0200

    add header template and adding nav bar functionality

--- fish-world/src/app/core/header/header.component.ts
+++ fish-world/src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router, 
+  private userService: UserService) {}
+
+  get isLoggedIn(): boolean {
+  return this.userService.isLogged;
+}
+
+logout(): void {
+  this.userService.logout();
+  this.router.navigate(['home'])
+}
 }
