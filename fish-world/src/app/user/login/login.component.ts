7f77e1c22b38026b580e0530ad455f2622735869
gyuzelova <tanq.nikolova.tn@gmail.com>
2024-03-29 23:06:49 +0200

    add  login functionality

--- fish-world/src/app/user/login/login.component.ts
+++ fish-world/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private userService: UserService, protected router: Router, private fb:FormBuilder) {}
+
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const { email, password } = this.form.value;
+
+    this.userService
+    .login(email!, password!)
+    .subscribe(() => {
+      this.router.navigate(['/home']);
+    });
+
+
+  }
+
 
 }
