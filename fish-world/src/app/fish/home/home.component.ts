321371c120f9c8ab16804e2eed87cca173a38691
gyuzelova <tanq.nikolova.tn@gmail.com>
2024-03-30 23:55:50 +0200

    create home page display 3 posts

--- fish-world/src/app/fish/home/home.component.ts
+++ fish-world/src/app/fish/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/app.service';
+import { Fish } from 'src/app/types/post';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  fishPost: Fish[]=[];
+
+constructor(private api: AppService) {}
+ngOnInit(): void {
+  this.api.getFishs().subscribe({
+next: (data) => {
+  this.fishPost = data;
+},
+error: (err) => {
+  console.log(err);
+  
+  }
+})
+}
 
 }
