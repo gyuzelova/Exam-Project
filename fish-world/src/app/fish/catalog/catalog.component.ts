aa3a50575e33a8da823a688b60ededd073dcd7be
gyuzelova <tanq.nikolova.tn@gmail.com>
2024-03-30 23:55:22 +0200

    create catalog page display all posts

--- fish-world/src/app/fish/catalog/catalog.component.ts
+++ fish-world/src/app/fish/catalog/catalog.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/app.service';
+import { Fish } from 'src/app/types/post';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  fishPost: Fish[]=[];
+  
+  constructor(private api: AppService) {}
 
+  ngOnInit(): void {
+    this.api.getAllFishs().subscribe({
+  next: (data) => {
+    this.fishPost = data;
+    console.log(this.fishPost);
+    
+  },
+  error: (err) => {
+    console.log(err);
+    
+    }
+  })
+  }
 }
