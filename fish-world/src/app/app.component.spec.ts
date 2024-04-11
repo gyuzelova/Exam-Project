ef8b5bfd330316ceccc96d66d4d30b99124b0026
gyuzelova <tanq.nikolova.tn@gmail.com>
2024-04-11 15:09:04 +0300

    update test component

--- fish-world/src/app/app.component.spec.ts
+++ fish-world/src/app/app.component.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+import { FishModule } from './fish/fish.module';
+import { UserModule } from './user/user.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppInterceptor } from './app.interceptor';
+import { SharedModule } from './shared/shared.module';
 
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule,  BrowserModule,
+      CoreModule,
+      FishModule,
+      UserModule,
+      SharedModule,
+      HttpClientModule,
+      AppRoutingModule],
+    declarations: [AppComponent],
+    providers: [{ useClass: AppInterceptor,
+      multi: true,
+      provide: HTTP_INTERCEPTORS,}]
   }));
 
   it('should create the app', () => {
@@ -15,10 +32,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'culinary-recipes'`, () => {
+  it(`should have as title 'Fish world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('culinary-recipes');
+    expect(app.title).toEqual('fish-world');
   });
 
   it('should render title', () => {
